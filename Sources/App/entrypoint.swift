a36c2e4e208ad05fd166d078105e85293248bc0d
glasfisch3000 <jakob@danckwerts.net>
2024-01-28 22:58:20 +0100

    combined entrypoint and configure

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -19,3 +19,12 @@ enum Entrypoint {
         try await app.execute()
     }
 }
+
+// configures your application
+public func configure(_ app: Application) async throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    // register routes
+    try routes(app)
+}
