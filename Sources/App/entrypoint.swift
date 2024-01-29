8db0441f666c1a17c122af2e894ac1b6ffda9e8f
glasfisch3000 <jakob@danckwerts.net>
2024-01-29 20:59:17 +0100

    nothing important

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -23,10 +23,8 @@ enum Entrypoint {
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.http.server.configuration.port = 4096
 
     // register routes
     try routes(app)
-    app.http.server.configuration.port = 4096
 }
