870c636394dc297e3865c90994cc98f344c3ed3f
glasfisch3000 <jakob@danckwerts.net>
2024-02-06 22:52:54 +0100

    updated products

--- Package.swift
+++ Package.swift
@@ -7,7 +7,9 @@ let package = Package(
         .macOS("13.3")
     ],
     products: [
-        .library(name: "zenea-swift", targets: ["zenea", "zenea-fs", "zenea-http"])
+        .library(name: "zenea-swift", targets: ["zenea"]),
+        .library(name: "zenea-swift-fs", targets: ["zenea-fs"]),
+        .library(name: "zenea-swift-http", targets: ["zenea-http"])
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.92.1"),
