268586c2df577190c2e02e8eebb18e0a5722f591
glasfisch3000 <jakob@danckwerts.net>
2024-04-18 14:47:30 +0200

    added Block documentation

--- Sources/zenea/Block.swift
+++ Sources/zenea/Block.swift
@@ -1,10 +1,21 @@
 import Foundation
 import Crypto
 
+/// An implementation of a Zenea Data Layer block that encapsulates binary data in a content-identifiable way.
+///
+/// A block serves as the basis of Zenea data storage and handling.
+/// Each block stores an ID, which is a cryptographic hash of its entire content.
+///
+/// Create a block using ``init(id:content:)``. If no ID is provided, it will be computed from the given data.
+///
+/// Blocks can also be retreived from ``BlockStorage`` implementations.
 public struct Block {
     public typealias Content = Data
     
+    /// A cryptographic hash of the block's content that is used as an identifier.
     public var id: Self.ID
+    
+    /// The block's wrapped data.
     public var content: Content
     
     /// Create a block with a set of contained data and, optionally, a pre-computed ID. If none is provided, an ID will be created by hashing the given content.
@@ -22,6 +33,7 @@ public struct Block {
         }
     }
     
+    /// Re-computes the block's hash value and checks whether it matches the given ID.
     public func matchesID(_ id: ID) -> Bool {
         switch id.algorithm {
         case .sha2_256:
@@ -39,5 +51,6 @@ extension Block: Hashable { }
 extension Block: Codable { }
 
 extension Block {
+    /// The maximum byte size a block should have, as specified by the Zenea Project.
     public static let maxBytes: Int = 1<<16
 }
