d3578cbc994bbe10ab8751bcb6f7bc70886416bf
glasfisch3000 <jakob@danckwerts.net>
2024-04-23 14:35:02 +0200

    added BlockStorageBuilder buildBlock function

--- Sources/zenea/result-builder/BlockStorageBuilder.swift
+++ Sources/zenea/result-builder/BlockStorageBuilder.swift
@@ -20,4 +20,8 @@ public enum BlockStorageBuilder {
     public static func buildEither<Component>(second component: Component) -> some BlockStorage where Component: BlockStorage {
         component
     }
+    
+    public static func buildBlock<C1, C2>(_ c1: C1, _ c2: C2) -> some BlockStorage where C1: BlockStorage, C2: BlockStorage {
+        BlockStorageTuple(source1: c1, source2: c2)
+    }
 }
